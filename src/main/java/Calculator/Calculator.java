edaa28dddf3442329da49192c2c30fb1c0243bba
Mario Fraile <mariofrailedev@gmail.com>
2019-07-11 12:12:14 -0500

    set up project

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public class Calculator {
+
+    public static int sum(int n1, int n2){
+        return n1 + n2;
+    }
+
+    public static int rest(int n1, int n2){
+        return n1 - n2;
+    }
+}
